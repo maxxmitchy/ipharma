fc0a9ce65052488b42f9264a0c0bc79203bd7d51
MAxMitCHy <mitchycarl6@gmail.com>
2021-11-08 21:57:52 +0100

    used optional helper

--- app/Http/Livewire/Cartcounter.php
+++ app/Http/Livewire/Cartcounter.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Cart;
 use Livewire\Component;
 
 class Cartcounter extends Component
@@ -14,6 +13,6 @@ class Cartcounter extends Component
 
     public function render()
     {
-        return view('livewire.cartcounter', ['cartCount' => session()->has('shopping-cart') ? session('shopping-cart')->sum('quantity') : 0]);
+        return view('livewire.cartcounter', ['cartCount' => optional(session('shopping-cart'))->sum('quantity') ?? 0]);
     }
 }
